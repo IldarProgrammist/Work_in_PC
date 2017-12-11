29f6901c4762fe97ec88723c0378b0828ca33d8c
Ildar.Urazbakhtin <programmerildarik@gmail.com>
2017-12-11 16:49:20 +0300

    Добавлена два новых пункта имя компьютера и имя пользователя

--- ip_machine.py
+++ ip_machine.py
@@ -1,12 +1,16 @@
 import socket
+import getpass
 
-#Функция для определения сетевого ip-адреса на компьютере
-def Inforamtion_of_ip():
+#Функция Информация о компьютере
+def Inforamtion_of_machine():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(('mail.ru', 80))
-    print('Ip-адрес данного компьютера: '+ s.getsockname()[0])
+    print('Имя компьютера'+ socket.gethostname())
+    print('Имя пользователя:' + getpass.getuser())
+    print('Ip-адрес компьютера: '+ s.getsockname()[0])
+
     s.close()
-Inforamtion_of_ip()
+Inforamtion_of_machine()
 
 
 
